22d0260b5f598039f746de5b5218eedfaca070da
Keyi Xie <xiekeyi98@gmail.com>
2018-12-21 12:55:48 +0800

    style:add comment

--- src/main.cpp
+++ src/main.cpp
@@ -81,7 +81,7 @@ void output( const int max_size )
 				if( !fin.is_open() )
 						continue;
 				string s ;
-				map<string,int> mp; //TODO 
+				map<string,int> mp; //TODO unordered_map
 				while( fin >> s )
 						mp[s]++;
 				ofstream fout(output_name , ios::app);
